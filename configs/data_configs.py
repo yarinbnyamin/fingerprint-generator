761f4e9c45f39f84e89386165436359432dff9df
Окунев Даниил Александрович <dokunev@phoenixit.ru>
2024-04-25 15:13:03 +0300

    fix paths for data

--- configs/data_configs.py
+++ configs/data_configs.py
@@ -3,30 +3,9 @@ from configs.paths_config import dataset_paths
 
 
 DATASETS = {
-	'nist_sd14_mnt': {
-		'transforms': transforms_config.MntToFingerTransforms,
-		'train_source_root': dataset_paths['nist_sd14_mnt_train'],
-		'train_target_root': dataset_paths['nist_sd14_mnt_gt_train'],
-		'test_source_root': dataset_paths['nist_sd14_mnt_test'],
-		'test_target_root': dataset_paths['nist_sd14_mnt_gt_test'],
-	},
-	'nist_sd4_mnt': {
-		'transforms': transforms_config.MntToFingerTransforms,
-		'train_source_root': dataset_paths['nist_sd4_mnt_train'],
-		'train_target_root': dataset_paths['nist_sd4_mnt_gt_train'],
-		'test_source_root': dataset_paths['nist_sd4_mnt_test'],
-		'test_target_root': dataset_paths['nist_sd4_mnt_gt_test'],
-	},
 	'nist_sd14_synthesis': {
 		'transforms': transforms_config.FingerprintSynthesisTransforms,
 		'train_target_root': dataset_paths['nist_sd14_gt_train'],
 		'test_target_root': dataset_paths['nist_sd14_gt_test'],
-	},
-	'nist_sd4_synthesis': {
-		'transforms': transforms_config.FingerprintSynthesisTransforms,
-		'train_source_root': dataset_paths['nist_sd4_mnt_train'],
-		'train_target_root': dataset_paths['nist_sd4_mnt_gt_train'],
-		'test_source_root': dataset_paths['nist_sd4_mnt_test'],
-		'test_target_root': dataset_paths['nist_sd4_mnt_gt_test'],
 	},
 }
