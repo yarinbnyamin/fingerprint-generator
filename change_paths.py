5c659f5d169736781534e0cc82f139fd2e4b5bde
Окунев Даниил Александрович <dokunev@phoenixit.ru>
2024-04-26 12:58:52 +0300

    fix change_paths

--- change_paths.py
+++ change_paths.py
@@ -2,30 +2,25 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-def update_paths_in_config(file_path, train_path, test_path):
+def update_path():
+    current_path = os.getcwd()
+    file_path = os.path.join(current_path, 'configs/paths_config.py')
     with open(file_path, 'r') as file:
         config_content = file.read()
+    train_path = os.path.join(current_path, "content/Africano/Train")
+    valid_path = os.path.join(current_path, "content/Africano/Valid")
 
     new_config_content = config_content.replace("'nist_sd14_gt_train': '',", f"'nist_sd14_gt_train': '{train_path}',")
-    new_config_content = new_config_content.replace("'nist_sd14_gt_test': '',", f"'nist_sd14_gt_test': '{test_path}',")
+    new_config_content = new_config_content.replace("'nist_sd14_gt_test': '',", f"'nist_sd14_gt_test': '{valid_path}',")
 
+    # Записываем изменения обратно в файл
     with open(file_path, 'w') as file:
         file.write(new_config_content)
 
 def main():
-    file_path = input("Введите путь к файлу: ")
+    update_path()
 
-    if not os.path.exists(file_path):
-        print("Файл не найден.")
-        return
-
-    train_path = input("Введите новый путь для Train: ")
-    test_path = input("Введите новый путь для Valid: ")
-
-    # Обновляем пути в файле
-    update_paths_in_config(file_path, train_path, test_path)
-
-    print("Пути успешно обновлены в файле.")
+    print("Пути успешно обновлены в файле configs/paths_config.py.")
 
 if __name__ == "__main__":
     main()
